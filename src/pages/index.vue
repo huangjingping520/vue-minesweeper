11002371750e056c58de0014bc93c3e11a8c310a
huangjingping520 <6109119167@email.ncu.edu.cn>
2022-03-19 23:46:29 +0800

    components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,141 +1,9 @@
 <script setup lang="ts">
-import type { BlockState } from '~/types'
-const WEIGHT = 10
-const HEIGHT = 10
-const state = ref(
-  Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WEIGHT },
-      (_, x): BlockState => ({
-        x,
-        y,
-        adjacentMines: 0,
-        revealed: false,
-      }),
-    ),
-  ),
-)
+import { isDev, toggleDev } from '~/composables'
+import { GamePlay } from '~/composables/logic'
 
-function generateMines(initial: BlockState) {
-  for (const row of state.value) {
-    for (const block of row) {
-      if (Math.abs(initial.x - block.x) <= 1)
-        continue
-      if (Math.abs(initial.y - block.y) <= 1)
-        continue
-      block.mine = Math.random() < 0.2
-    }
-  }
-  updateNumbers()
-}
-
-const directions = [
-  [1, 1],
-  [1, 0],
-  [1, -1],
-  [0, -1],
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, 1],
-]
-
-const numberColors = [
-  'text-transparent',
-  'text-gray-500',
-  'text-blue-500',
-  'text-green-500',
-  'text-yellow-500',
-  'text-orange-500',
-  'text-red-500',
-  'text-purple-500',
-  'text-pink-500',
-]
-
-function updateNumbers() {
-  state.value.forEach((row, y) => {
-    row.forEach((block, x) => {
-      if (block.mine)
-        return
-      getSiblings(block)
-        .forEach((b) => {
-          if (b.mine)
-            block.adjacentMines += 1
-        })
-    })
-  })
-}
-
-function expendZero(block: BlockState) {
-  if (block.adjacentMines)
-    return
-  getSiblings(block).forEach((s) => {
-    if (!s.revealed) {
-      s.revealed = true
-      expendZero(s)
-    }
-  })
-}
-
-let mineGenerated = false
-const dev = false
-
-function onRightClick(block: BlockState) {
-  if (block.revealed)
-    return
-  block.flagged = !block.flagged
-  // checkGameState()
-}
-
-function onClick(e: MouseEvent, block: BlockState) {
-  if (!mineGenerated) {
-    generateMines(block)
-    mineGenerated = true
-  }
-  block.revealed = true
-  if (block.mine)
-    alert('BOOOOM!')
-
-  expendZero(block)
-  // checkGameState()
-}
-
-function getBlockClass(block: BlockState) {
-  if (block.flagged)
-    return 'bg-gray-500/10'
-  if (!block.revealed)
-    return 'bg-gray-500/10 hover:bg-gray-500/20'
-  return block.mine ? 'text-red' : numberColors[block.adjacentMines]
-}
-
-// generateMines()
-// updateNumbers()
-
-function getSiblings(block: BlockState) {
-  return directions.map(([dx, dy]) => {
-    const x2 = block.x + dx
-    const y2 = block.y + dy
-    if (x2 < 0 || x2 >= WEIGHT || y2 < 0 || y2 >= HEIGHT)
-      return undefined
-
-    return state.value[y2][x2]
-  })
-    .filter(Boolean) as BlockState[]
-}
-
-watchEffect(checkGameState)
-
-function checkGameState() {
-  if (!mineGenerated)
-    return
-  const blocks = state.value.flat()
-
-  if (blocks.every(block => block.revealed || block.flagged)) {
-    if (blocks.some(block => block.flagged && !block.mine))
-      alert('You Cheat!')
-    else
-      alert('You win!')
-  }
-}
+const play = new GamePlay(12, 12)
+const state = play.state
 </script>
 
 <template>
@@ -148,27 +16,21 @@ function checkGameState() {
         flex="~"
         items-center justify-center
       >
-        <button
+        <MineBlock
           v-for="block, x in row" :key="x"
-          flex="~"
-          items-center justify-center
-          w-10 h-10 m="0.5"
-          border="1 gray-300"
-          :class="getBlockClass(block)"
-          @click="onClick($event, block)"
-          @contextmenu.prevent="onRightClick(block)"
-        >
-          <template v-if="block.flagged">
-            <div i-mdi-flag text-red />
-          </template>
-          <template v-else-if="block.revealed || dev">
-            <div v-if="block.mine" i-mdi:mine />
-            <div v-else>
-              {{ block.adjacentMines }}
-            </div>
-          </template>
-        </button>
+          :block="block"
+          @click="play.onClick(block)"
+          @contextmenu.prevent="play.onRightClick(block)"
+        />
       </div>
+    </div>
+    <div flex="~ gap-1" justify-center>
+      <button btn @click="toggleDev()">
+        {{ isDev ? 'DEV' : 'NORMAL' }}
+      </button>
+      <button btn @click="play.reset()">
+        RESET
+      </button>
     </div>
   </div>
 </template>
